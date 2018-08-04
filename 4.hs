93960ba9f81d08d8289f1dd5039b5cbc882acc00
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-08-04 16:20:33 +0900

    finished 4

---
+++ 4.hs
@@ -0,0 +1,15 @@
+-- 3桁の積の和
+-- 数値反転 reverse int
+
+reverseInt :: Integer -> Integer
+reverseInt x | x < 0     = 0 - (read . reverse . tail . show $ x)
+             | otherwise = read . reverse . show $ x
+
+check :: Integer -> Integer
+check a = if (a == reverseInt a) then a else 0
+
+main = do
+  print $ check 9009
+  print $ check 9019
+  let initList = [100,101..999]
+  print $ maximum $ fmap check $ (*) <$> initList <*> initList
